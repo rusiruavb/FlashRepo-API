5934a7e7463759953877d7925e5bc1535fa7d8e6
Rusiru Abhisheak <rusiruavb98@gmail.com>
2021-01-06 14:45:00 +0530

    Finish the flash card database model

--- models/card.model.js
+++ models/card.model.js
@@ -1,7 +1,45 @@
 const mongoose = require("mongoose");
 
 const CardSchema = new mongoose.Schema({
-
+    userID: {
+        type: mongoose.Schema.Types.ObjectId,
+        required: true,
+        ref: "users"
+    },
+    subjectName: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    lessonName: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    description: {
+        type: String,
+        required: false,
+        trim: true
+    },
+    question: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    answer: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    date: {
+        type: new Date(),
+        required: false
+    },
+    status: {
+        type: String,
+        required: true,
+        trim: true
+    }
 })
 
 const Card = mongoose.model("cards", CardSchema);
